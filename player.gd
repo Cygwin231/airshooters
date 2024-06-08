5e192721df2942695ada8674ad8f1cbf64e9afbb
TheBlueTophatm <tbtproductionswork@gmail.com>
2024-06-08 19:27:18 -0400

    Modified player, map scenes

--- player.gd
+++ player.gd
@@ -7,8 +7,16 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var camera = $Camera3D
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	
+	
+	
+	#camera.rotate_z(0.1)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -24,6 +32,8 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		camera.rotate_z(direction.x * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
